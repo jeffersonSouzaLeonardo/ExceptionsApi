72c1f6437c9503af27c8ed20c9514e726ca1db54
jeffersonleonardo123 <jeffersonleonardo@hotmail.com>
2025-08-20 11:38:17 -0300

    Extendendo o ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafoods/infra/repository/exception/ApiExecptionHandle.java
+++ src/main/java/com/algaworks/algafoods/infra/repository/exception/ApiExecptionHandle.java
@@ -3,11 +3,12 @@ package com.algaworks.algafoods.infra.repository.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExecptionHandle {
+public class ApiExecptionHandle extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<?> responseException(NegocioException e){
--- src/main/java/com/algaworks/algafoods/infra/repository/exception/CidadeException.java
+++ src/main/java/com/algaworks/algafoods/infra/repository/exception/CidadeException.java
@@ -2,6 +2,8 @@ package com.algaworks.algafoods.infra.repository.exception;
 
 public class CidadeException extends Exception {
 
+
+
     public CidadeException(String mensagem){
         super(mensagem);
     }
